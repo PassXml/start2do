ef787cf74d2e836c405acbbfee7cc7d7a90aa936
Kiki <hellobox@outlook.com>
2022-12-28 15:24:53 +0800

    update

--- spring-common/src/main/java/org/start2do/wrapper/ReplaceRequestFilter.java
+++ spring-common/src/main/java/org/start2do/wrapper/ReplaceRequestFilter.java
@@ -7,18 +7,12 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.ContentCachingRequestWrapper;
-import org.springframework.web.util.ContentCachingResponseWrapper;
 
-@Component
 public class ReplaceRequestFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
         throws IOException, ServletException {
-        chain.doFilter(new ContentCachingRequestWrapper((HttpServletRequest) request),
-            new ContentCachingResponseWrapper((HttpServletResponse) response));
+        chain.doFilter(new MyRequestWrapper((HttpServletRequest) request), response);
     }
 }
