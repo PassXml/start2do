a335c6fb53f73d60542f7301c980603077b4e41e
李杰 <li.jie@zxrail.com.cn>
2023-08-25 16:13:09 +0800

    fix

--- muilt-datasource-primary/src/main/java/org/start2do/MultiDatasourcePrimaryAutoConfig.java
+++ muilt-datasource-primary/src/main/java/org/start2do/MultiDatasourcePrimaryAutoConfig.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
@@ -31,12 +32,12 @@ public class MultiDatasourcePrimaryAutoConfig {
 
     public final static String MainSqlSessionFactory = "MainSqlSessionFactory";
 
-//    @Primary
-//    @Bean("MainDataSource")
-//    public DataSource dataSource(MultiDatasourcePrimaryConfig config) {
-//        return DataSourceBuilder.create().url(config.getUrl()).driverClassName(config.getDriverClassName())
-//            .type(config.getType()).username(config.getUsername()).password(config.getPassword()).build();
-//    }
+    @Primary
+    @Bean("dataSource")
+    public DataSource dataSource(MultiDatasourcePrimaryConfig config) {
+        return DataSourceBuilder.create().url(config.getUrl()).driverClassName(config.getDriverClassName())
+            .type(config.getType()).username(config.getUsername()).password(config.getPassword()).build();
+    }
 
     @Primary
     @Bean("MainSqlSessionFactory")
