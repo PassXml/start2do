0e6c0c939edcc4d6d88177777232297967f73992
lijie <li.jie@zxrail.com.cn>
2024-11-25 16:06:32 +0800

    更新Spring Boot版本至2.7.18，更新相关依赖项版本，移除部分无用的注释与配置，优化Ebean自动配置类中的条件逻辑。

--- ebean-common/src/main/java/org/start2do/EbeanBeanAutoConfiguration.java
+++ ebean-common/src/main/java/org/start2do/EbeanBeanAutoConfiguration.java
@@ -1,22 +1,30 @@
 package org.start2do;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.ebean.Database;
 import io.ebean.DatabaseFactory;
 import io.ebean.config.CurrentUserProvider;
-import io.ebean.config.DatabaseConfig;
 import io.ebean.migration.MigrationConfig;
 import io.ebean.migration.MigrationRunner;
 import io.ebean.spring.txn.SpringJdbcTransactionManager;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.start2do.ebean.id_generators.SnowflakeStrGenerator;
 import org.start2do.ebean.id_generators.UUIDStrIdGenerator;
 import org.start2do.ebean.service.SysSettingService;
@@ -26,9 +34,6 @@ import org.start2do.util.Snowflake;
 @Import(EbeanConfig.class)
 @ConditionalOnProperty(name = "spring.datasource.url")
 @RequiredArgsConstructor
-@ComponentScans(value = {
-    @ComponentScan(value = "org.start2do.ebean"),
-})
 public class EbeanBeanAutoConfiguration {
 
 
@@ -41,15 +46,17 @@ public class EbeanBeanAutoConfiguration {
         runner.run(dataSource);
     }
 
-    @ConditionalOnBean(value = {ObjectMapper.class})
-    @ConditionalOnMissingBean(DatabaseConfig.class)
-    public static class Config1 {
-
+    @Order
+    @Configuration
+    @AutoConfigureAfter(DatasourceConfig.class)
+    public static class DatabaseConfig {
 
         @Bean
-        public DatabaseConfig databaseConfig(DataSource dataSource, CurrentUserProvider currentUserProvider,
+        @Conditional(DefaultDatabaseConfig2.class)
+        public io.ebean.config.DatabaseConfig databaseConfig(DataSource dataSource,
+            CurrentUserProvider currentUserProvider,
             ObjectMapper objectMapper, EbeanConfig ebeanConfig) {
-            DatabaseConfig config = new DatabaseConfig();
+            io.ebean.config.DatabaseConfig config = new io.ebean.config.DatabaseConfig();
             config.loadFromProperties();
             config.add(new UUIDStrIdGenerator());
             config.setCurrentUserProvider(currentUserProvider);
@@ -64,55 +71,19 @@ public class EbeanBeanAutoConfiguration {
             config.setObjectMapper(objectMapper);
             return config;
         }
-    }
-
-
-    @ConditionalOnMissingBean({DatabaseConfig.class, ObjectMapper.class})
-    public static class Config2 {
 
         @Bean
-        public DatabaseConfig databaseConfig(DataSource dataSource, CurrentUserProvider currentUserProvider,
-            EbeanConfig ebeanConfig) {
-            DatabaseConfig config = new DatabaseConfig();
-            config.loadFromProperties();
-            config.add(new UUIDStrIdGenerator());
-            config.setCurrentUserProvider(currentUserProvider);
-            config.setRunMigration(ebeanConfig.isMigration());
-            config.setDataSource(dataSource);
-            config.setDdlRun(false);
-            config.setExternalTransactionManager(new SpringJdbcTransactionManager());
-            config.setDdlCreateOnly(false);
-            if (ebeanConfig.isMigration()) {
-                EbeanBeanAutoConfiguration.migration(dataSource, ebeanConfig);
-            }
-            return config;
+        @Conditional(DefaultDatabase.class)
+        public io.ebean.Database database(io.ebean.config.DatabaseConfig config) {
+            return DatabaseFactory.create(config);
         }
 
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(CurrentUserProvider.class)
-    public CurrentUserProvider currentUserProvider() {
-        return () -> "not set";
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(Database.class)
-    @ConditionalOnBean(value = {DatabaseConfig.class})
-    public io.ebean.Database database(DatabaseConfig config) {
-        return DatabaseFactory.create(config);
-    }
-
-
-    @ConditionalOnBean(value = {ObjectMapper.class, Snowflake.class})
-    @ConditionalOnMissingBean(DatabaseConfig.class)
-    public static class Config3 {
-
-
         @Bean
-        public DatabaseConfig databaseConfig(DataSource dataSource, CurrentUserProvider currentUserProvider,
+        @Conditional(DatabaseConfigDefault.class)
+        public io.ebean.config.DatabaseConfig databaseConfig(DataSource dataSource,
+            CurrentUserProvider currentUserProvider,
             ObjectMapper objectMapper, EbeanConfig ebeanConfig, Snowflake snowflake) {
-            DatabaseConfig config = new DatabaseConfig();
+            io.ebean.config.DatabaseConfig config = new io.ebean.config.DatabaseConfig();
             config.loadFromProperties();
             config.add(new UUIDStrIdGenerator());
             config.add(new SnowflakeStrGenerator(snowflake));
@@ -128,11 +99,126 @@ public class EbeanBeanAutoConfiguration {
             config.setObjectMapper(objectMapper);
             return config;
         }
+
+        @Bean
+        @ConditionalOnBean(Database.class)
+        public SysSettingUtil sysSettingUtil(SysSettingService sysSettingService) {
+            return new SysSettingUtil(sysSettingService);
+        }
+
+        @Bean
+        @ConditionalOnMissingBean({io.ebean.config.DatabaseConfig.class, ObjectMapper.class})
+        public io.ebean.config.DatabaseConfig databaseConfig(DataSource dataSource,
+            CurrentUserProvider currentUserProvider,
+            EbeanConfig ebeanConfig) {
+            io.ebean.config.DatabaseConfig config = new io.ebean.config.DatabaseConfig();
+            config.loadFromProperties();
+            config.add(new UUIDStrIdGenerator());
+            config.setCurrentUserProvider(currentUserProvider);
+            config.setRunMigration(ebeanConfig.isMigration());
+            config.setDataSource(dataSource);
+            config.setDdlRun(false);
+            config.setExternalTransactionManager(new SpringJdbcTransactionManager());
+            config.setDdlCreateOnly(false);
+            if (ebeanConfig.isMigration()) {
+                EbeanBeanAutoConfiguration.migration(dataSource, ebeanConfig);
+            }
+            return config;
+        }
     }
 
-    @Bean
-    @ConditionalOnProperty(prefix = "spring.datasource", name = "url")
-    public SysSettingUtil sysSettingUtil(SysSettingService sysSettingService) {
-        return new SysSettingUtil(sysSettingService);
+    public static class DefaultDatabaseConfig2 implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return hasBean(context, ObjectMapper.class) && !hasBean(context, io.ebean.config.DatabaseConfig.class);
+
+        }
     }
+
+
+    @Bean
+    @ConditionalOnMissingBean(CurrentUserProvider.class)
+    public CurrentUserProvider currentUserProvider() {
+        return () -> "not set";
+    }
+
+
+    public static class DefaultDatabase implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return hasBean(context, io.ebean.config.DatabaseConfig.class) && !hasBean(context, Database.class);
+
+        }
+    }
+
+
+    public static class DatabaseConfigDefault implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return hasBean(context, ObjectMapper.class) && hasBean(context, Snowflake.class) && !hasBean(context,
+                io.ebean.config.DatabaseConfig.class);
+
+        }
+    }
+
+    public static boolean hasBean(ConditionContext context, Class<?> clazz) {
+        try {
+            Object bean = context.getBeanFactory().getBean(clazz);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Configuration
+    @AutoConfigureBefore(DatabaseConfig.class)
+    public static class DatasourceConfig {
+
+        @Bean
+        @Conditional(DefaultDataSource.class)
+        public DataSource dataSource(DataSourceProperties property) {
+            HikariConfig config = new HikariConfig();
+            config.setUsername(property.getUsername());
+            config.setPassword(property.getPassword());
+            config.setJdbcUrl(property.getUrl());
+            config.setDriverClassName(property.getDriverClassName());
+            return new HikariDataSource(config);
+        }
+
+        @Bean
+        @Conditional(DefaultDataSource2.class)
+        public DataSource dataSource(HikariConfig config) {
+            return new HikariDataSource(config);
+        }
+
+
+    }
+
+
+    public static class DefaultDataSource2 implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            boolean hasDataSource = hasBean(context, HikariDataSource.class);
+            boolean hasConfig = hasBean(context, HikariConfig.class);
+            return hasDataSource && hasConfig;
+        }
+    }
+
+
+    public static class DefaultDataSource implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            boolean hasDataSource = hasBean(context, DataSource.class);
+            boolean hasConfig = hasBean(context, HikariConfig.class);
+            String property = context.getEnvironment().getProperty("spring.datasource.url");
+
+            return !hasDataSource && !hasConfig && property != null;
+        }
+    }
+
 }
--- spring-business/src/main/java/org/start2do/service/SysDeptService.java
+++ spring-business/src/main/java/org/start2do/service/SysDeptService.java
@@ -1,8 +1,6 @@
 package org.start2do.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.start2do.dto.BusinessException;
 import org.start2do.ebean.service.AbsService;
@@ -12,8 +10,6 @@ import org.start2do.entity.security.query.QSysUser;
 
 @Service
 @RequiredArgsConstructor
-@EnableConfigurationProperties({DataSourceProperties.class})
-
 public class SysDeptService extends AbsService<SysDept> {
 
     private final SysUserService sysUserService;
