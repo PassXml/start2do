70507f42f888068d5c80cc5bd553272f0056ed76
lijie <li.jie@zxrail.com.cn>
2025-06-16 17:14:04 +0800

    feat: 添加CEPRule类及PatternStep内部类实现

---
+++ cep-util/src/main/java/org/start2do/cep/dto/CEPRule.java
@@ -0,0 +1,52 @@
+package org.start2do.cep.dto;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@Setter
+@Getter
+@Accessors(chain = true)
+@NoArgsConstructor
+@com.fasterxml.jackson.annotation.JsonIgnoreProperties(ignoreUnknown = true)
+@JsonSerialize
+@com.fasterxml.jackson.databind.annotation.JsonSerialize
+@ToString
+public class CEPRule {
+
+    private String ruleId;
+    private String ruleName;
+    private String description;
+    private boolean enabled;
+    private List<PatternStep> pattern;
+    private Map<String, Object> conditions;
+    private long timeWindowSeconds;
+    private String action;
+    private Map<String, Object> actionBody;
+
+    // 模式步骤定义
+    @Setter
+    @Getter
+    @Accessors(chain = true)
+    @NoArgsConstructor
+    @com.fasterxml.jackson.annotation.JsonIgnoreProperties(ignoreUnknown = true)
+    @com.fasterxml.jackson.databind.annotation.JsonSerialize
+    @JsonSerialize
+    public static class PatternStep implements Serializable {
+
+        private String stepName;
+        private String eventType;
+        private Map<String, Object> filters;
+        private PatternQuantifier quantifier;
+        private long timeoutSeconds;
+
+    }
+
+
+}
