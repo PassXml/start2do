e014c1fda6e9e57c8187662385da647331b58a52
lijie <passxml@gmail.com>
2026-06-17 08:50:39 +0800

    chore: 更新脚本默认白名单类
    扩展脚本默认白名单，新增基础类型、集合接口及常用工具类，
    以支持更多安全的脚本调用场景。

--- script-util/script-common/src/main/java/org/start2do/script/util/ScriptWhiteList.java
+++ script-util/script-common/src/main/java/org/start2do/script/util/ScriptWhiteList.java
@@ -8,10 +8,17 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import org.start2do.script.util.impl.functions.DBOperateFunction;
@@ -22,17 +29,34 @@ import org.start2do.script.util.impl.functions.SqlOperateFunction;
 public final class ScriptWhiteList {
 
     private static final Set<String> DEFAULT_WHITE_LIST = new LinkedHashSet<>(Arrays.asList(
+        Object.class.getName(),
         String.class.getName(),
+        CharSequence.class.getName(),
         Boolean.class.getName(),
+        Byte.class.getName(),
+        Character.class.getName(),
+        Number.class.getName(),
         Integer.class.getName(),
         Long.class.getName(),
         Double.class.getName(),
         Float.class.getName(),
         Short.class.getName(),
+        StringBuilder.class.getName(),
+        StringBuffer.class.getName(),
+        Comparable.class.getName(),
+        Iterable.class.getName(),
         BigDecimal.class.getName(),
         BigInteger.class.getName(),
         Math.class.getName(),
         UUID.class.getName(),
+        Collection.class.getName(),
+        List.class.getName(),
+        Map.class.getName(),
+        Set.class.getName(),
+        Iterator.class.getName(),
+        Collections.class.getName(),
+        Objects.class.getName(),
+        Optional.class.getName(),
         ArrayList.class.getName(),
         HashMap.class.getName(),
         HashSet.class.getName(),
